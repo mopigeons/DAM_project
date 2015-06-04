4f3fab54ee7d9a1b82ee83748b96f094edb15508
simon rodier <simon@mopigeons.com>
2015-06-04 16:31:00 -0400

    implémentation du classificateur de base (svm_fr)

--- baseClassification.py
+++ baseClassification.py
@@ -2,9 +2,13 @@ __author__ = 'Simon'
 
 from sklearn import svm
 import utility as ut
+import os
+import numpy as np
+import scipy as sp
+from scipy.sparse import csc_matrix
 
 def run_svm_fr(data):
-    print("svm fr")
+    print("***---***---***   svm_fr   ***---***---***")
     # setpaths? will it be necessary to go get other data?
     # parametres
     C = 1
@@ -21,5 +25,27 @@ def run_svm_fr(data):
 
 
 def main_svm_fr(data, C, kernel_types, kernel_params):
+    results_directory_name = "results"
+    result_file = os.path.join(results_directory_name, "svm_fr", "result_main_svm_fr.txt")
+    # crée le répertoire "results" et le sous-répertoire "svm_fr"
+    if not (os.path.exists(results_directory_name)):
+        os.mkdir(results_directory_name)
+    if not (os.path.exists(os.path.join(results_directory_name, 'svm_fr'))):
+        os.mkdir(os.path.join(results_directory_name, 'svm_fr'))
+    # ici: on commence à la ligne 10 du fichier matlab
+    for s in range(len(data.Xsource)):
+        # création de répertoires results/svm_fr/decision_values/U00, U01, U02..
+        dv_dir = os.path.join(results_directory_name, 'svm_fr', 'decision_values', data.domain_names[s])
+        if not (os.path.exists(results_directory_name)):
+            os.mkdir(results_directory_name)
+        Xsource = data.Xsource[s]
+        ysource = data.ysource[s]
+        Xsparse = sp.sparse.vstack([data.Xtarget[0], csc_matrix(Xsource)])
+        S = Xsparse*Xsparse.transpose()
+        S = S.todense()
+        y = np.concatenate((data.ytarget[0], ysource))
+        src_index = [i + data.Xtarget[0].shape[0] for i in range(Xsource.shape[0])]
+        tar_index = [i for i in range(data.Xtarget[0].shape[0])]
+        # passé ici: ligne 26+ du code matlab
 
-    return "a string of words"  # todo: changer le retour
+    return "a string of words that marks the end of this function"  # todo: changer le retour

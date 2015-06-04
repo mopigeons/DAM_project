6d31330ccacc6132ab31c1b57f467b857c2b9ae7
simon rodier <simon@mopigeons.com>
2015-06-04 11:49:07 -0400

    nettoyage de code

--- main.py
+++ main.py
@@ -5,17 +5,10 @@ import utility as ut
 import univerdam as un
 
 
-
 def main():
     data = ut.load_matlab_data()
     # todo: why is there a random permutation in matlab load_data file (lines 20-24)?
     un.launch_univerdam(data)
-
-    # todo:
-    #run each s_i through svm
-    #(keep results)
-    # [continue]
-
 
 
 if __name__ == "__main__":
